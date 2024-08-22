8b94a1b97ed88b0cd4d0cedf1c57fd9785f23954
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2024-08-22 16:56:50 -0500

    Add title to Streamlit app

--- App.py
+++ App.py
@@ -4,3 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import plotly.express as px 
+
+
+# Título de la app
+st.title("K-Means Clustering con Análisis PCA usando Streamlit")
