3be156cf0cfbd7d8e59d0e97bc66fc35c3b75f62
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2024-08-22 16:50:20 -0500

    Ver1

--- Sem08.py
+++ Sem08.py
@@ -1,8 +1,1 @@
-import streamlit as st
-import pandas as pd
 
-# Título de la App
-st.title("K-Means Clustering con Análisis PCA usando Streamlit")
-
-# Subir archivo de Excel
-uploaded_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
