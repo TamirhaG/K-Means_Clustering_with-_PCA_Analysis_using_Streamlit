98a87c9d1d7aae8ec31f8d68ce929b756ed7f363
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2024-08-23 15:48:05 -0500

    Displayed data preview after file upload

--- stApp1.py
+++ stApp1.py
@@ -11,3 +11,9 @@ st.title("K-Means Clustering with PCA Analysis using Streamlit")
 
 # Upload Excel file
 uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+
+if uploaded_file is not None: 
+    df = pd.read_excel(uploaded_file)
+
+    st.write("### Data preview")
+    st.write(df.head())
