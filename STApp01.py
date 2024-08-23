42ffbd8a63d4d82ba35f4a95013cf0486b817cc2
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2024-08-23 11:15:05 -0500

    V1

--- STApp01.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-import plotly.express as px
