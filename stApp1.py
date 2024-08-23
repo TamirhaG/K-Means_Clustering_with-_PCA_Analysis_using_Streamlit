366486ac2c35908a67ecf8c336c30187426660b5
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2024-08-23 15:43:34 -0500

    Imported necessary libraries for Streamlit app

---
+++ stApp1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import plotly.express as px
+
